05944cd83440ffacbd693e9baf7b6c7a2cca7507
imrul <imrul@imrul-U30Jc.(none)>
2014-11-05 00:10:31 +0700

    NETWORK_PROJECT: code base...

---
+++ src/java/com/medicalCalculator/database/operation/Users.java
@@ -0,0 +1,70 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.medicalCalculator.database.operation;
+
+/**
+ *
+ * @author imrul
+ */
+public class Users {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    public Users() {
+        
+    }
+
+    public Users(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+}
